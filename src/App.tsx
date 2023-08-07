3b29f0d89c4f52266dbd8af6e9e6f980cac84525
shivvamm <shivam574488@gmail.com>
2023-08-08 02:01:57 +0530

    updated routing in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Home, Template, Uplaoad } from "./components/index"
 import './App.css'
 
 function App() {
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-black sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-        <p>Copyresume</p>
-      </header>
-      <div className='flex items-center justify-center h-auto'>
-        <BrowserRouter>
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-black sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+          <Link to="/">
+            <p>Copyresume</p>
+          </Link>
+        </header>
+        <div className='flex items-center justify-center h-auto'>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/template" element={<Template />} />
             <Route path="/upload" element={<Uplaoad />} />
           </Routes>
-        </BrowserRouter>
-      </div>
+        </div >
+      </BrowserRouter >
     </>
   )
 }
