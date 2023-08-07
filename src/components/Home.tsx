0e5619d806791e928c5c21bcfdc2ee9979995425
shivvamm <shivam574488@gmail.com>
2023-08-07 21:55:44 +0530

    updated home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import templateLogo from '../assets/template4.svg';
 import uploadLogo from '../assets/upload2.svg';
@@ -7,10 +6,12 @@ import '../App.css';
 function Home() {
     return (
         <div className="flex flex-col items-center justify-center h-auto w-full">
-            <div className="bg-gray-800 text-white p-6 rounded-md mb-6 mt-6 text-center">
-                <p className="text-xl font-semibold">
-                    The best Resume making tool you can find on the internet
-                    for creating a resume from a perfect template or uploading your own
+            <div className="w-2/3 px-8 py-4 bg-white rounded-lg mt-3 shadow-md dark:bg-gray-800 text-center">
+                <p className="text-3xl font-semibold">
+                    The Ultimate Resume Builder & Editor
+                </p>
+                <p className="font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200">
+                    Elevate your resume with our cutting-edge tools. Choose from templates or upload your own, all with an ATS-friendly touch.
                 </p>
             </div>
 
@@ -18,34 +19,34 @@ function Home() {
 
                 <Link to="/upload" className="mr-6 sm:mr-10">
                     <img src={uploadLogo} className="logo h-44 mt-4" alt="Upload and edit" />
-                    <p className="text-lg text-center font-semibold mb-6">Upload and Edit</p>
+                    <p className="text-lg text-center font-semibold mb-5">Upload and Edit</p>
                 </Link>
 
                 <Link to="/template">
                     <img src={templateLogo} className="logo h-44 mt-4" alt="Create from template" />
-                    <p className="text-lg text-center font-semibold  mb-6">Create from Template</p>
+                    <p className="text-lg text-center font-semibold  mb-5">Create from Template</p>
                 </Link>
 
             </div>
             <div className="flex flex-col items-center justify-center h-auto w-full">
                 <div className="flex w-full justify-center text-center">
-                    <div className="bg-gray-800 text-white p-6 rounded-md mr-1 w-1/3">
-                        <div className="mb-4">
-                            <p className="text-lg">Create and edit your resume by uploading a sample pdf format of your
-                                resume and if you have a word format its better to use microsoft word</p>
+                    <div className="bg-gray-800 text-white p-6 rounded-md mr-1 w-1/3 bg-white rounded-lg mt-6 shadow-md dark:bg-gray-800">
+                        <div className="mb-1">
+                            <p className="text-lg"> Craft your resume by uploading a PDF or utilizing Microsoft Word.
+                                Our tools ensure ATS compatibility and modern formatting.</p>
                         </div>
                     </div>
 
-                    <div className="bg-gray-800 text-white p-6 rounded-md ml-1 w-1/3">
-                        <div className="mb-4">
-                            <p className=" text-lg">Create and edit your resume by selecting from a big collection of best ATS socred
-                                resume that can increase your possibality of getting your dream job </p>
+                    <div className="bg-gray-800 text-white p-6 rounded-md ml-1 w-1/3 bg-white rounded-lg mt-6 shadow-md dark:bg-gray-800">
+                        <div className="mb-1">
+                            <p className=" text-lg"> Stand out with AI-enhanced templates. Select from a
+                                range of ATS-scored designs tailored to your industry and role.</p>
                         </div>
                     </div>
                 </div>
                 <hr className="border-t border-gray-500 w-1/2 mt-4 mb-2" />
             </div>
-        </div>
+        </div >
     );
 }
 
